df1b62d8763cb895dbc7a8d50de31de10833308e
Dilip Kasala <Dilip_Kasala@epam.com>
2018-06-11 15:37:27 +0530

    move task 2a and 2b files

---
+++ JavaEpam/src/javaPackage/Task2a.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Task2a 
+{
+	static int Daniel =3;
+	static int Amber =2;
+	static int Total;
+
+	public static void main(String[] args) 
+	{
+		
+		Total=Daniel+Amber;
+		System.out.println("Daniel and Amber have together have apples = " + Total);
+	
+	}
+	
+
+
+}
---
+++ JavaEpam/src/javaPackage/Task2b.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Task2b
+{
+	
+	static float Daniel =(float) 3.5;
+	static float Amber =(float) 2.5;
+	static float Total;
+
+	public static void main(String[] args)
+	{
+		
+		Total= (Daniel+Amber);
+		System.out.println("Daniel and Amber have together have apples = " + Total);
+
+	}
+
+}
