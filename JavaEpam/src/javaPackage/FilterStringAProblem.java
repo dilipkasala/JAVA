5bc739a25bce768c7fc39998150f789780f6ba88
dilipkasala <38006688+dilipkasala@users.noreply.github.com>
2018-09-09 17:34:05 +0530

    Commit Tasks 12

---
+++ JavaEpam/src/javaPackage/FilterStringAProblem.java
@@ -0,0 +1,20 @@
+package EpamPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FilterStringAProblem 
+{
+
+	public static void main(String[] args) 
+	{
+		List<String> arrList = Arrays.asList("Apple", "Ant", "Bat", "Baloon","Egg", "Eagle","Fish");
+        arrList.stream()
+        .filter((p) -> p.startsWith("A"))  
+        .filter(p -> p.length()==3)       
+        .forEach((p) -> System.out.println(p));
+
+	}
+
+}
